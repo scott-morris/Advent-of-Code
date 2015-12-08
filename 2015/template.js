31efbe4452d1b8baffb38e908397f695d2d4276f
Scott Morris <smorris@lb.com>
2015-12-08 13:48:23 -0500

    Added solution for day #6, stubs for days #7-8

---
+++ 2015/template.js
@@ -0,0 +1,16 @@
+/*
+	Problem description
+
+	PART ONE:
+	Describe answer #1
+
+	PART TWO:
+	Describe answer #2
+*/
+;(function (input) {
+	var answer1,
+		answer2;
+
+	console.log("Answer #1:", answer1);
+	console.log("Answer #2:", answer2);
+})(/* input */);
