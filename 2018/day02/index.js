102eb93d5c3a9b8961a6c00799db71cc25491178
Morris, Scott <SMorris@victoria.com>
2018-12-03 07:45:48 -0500

    Day 2 solution 1

--- 2018/day02/index.js
+++ 2018/day02/index.js
@@ -13,11 +13,40 @@ const testCases = require("../helpers/test-cases");
 
 // Private.
 
-/* put private functions here */
+const stringHasExactly = (str, num) => {
+	// Sort the characters to make it easier to count
+	const sortedChars = str.split("").sort().join("");
+
+	let charCount = 1;
+	for (let i = 1; i < sortedChars.length; i++) {
+		if (sortedChars[i-1] === sortedChars[i]) {
+			// If this character is the same as the previous, increment
+			charCount++;
+		} else {
+			// Otherwise, check to see if the count matches what we're looking for.
+			if (charCount === num) {
+				return true;
+			}
+			charCount = 1;
+		}
+	}
+
+	// Do one last check to see if the last character matches the number we want.
+	return (charCount === num);
+}
 
 // Solution #1
 const solution1 = (input) => {
-	/* put code to solve for part #1 here. */
+	let numHas2 = 0;
+	let numHas3 = 0;
+	input.forEach((str) => {
+		const has2 = stringHasExactly(str, 2);
+		const has3 = stringHasExactly(str, 3);
+
+		if (stringHasExactly(str, 2)) { numHas2++ };
+		if (stringHasExactly(str, 3)) { numHas3++ };
+	});
+	return numHas2 * numHas3;
 };
 
 // Solution #2
@@ -29,8 +58,29 @@ const solution2 = (input, answer1) => {
 
 // Define the tests that need to pass before running.
 const tests = [
-	() => { assert.equal(true, true) },
-	testCases.create(solution1, "input", "output")
+	() => { assert.equal(stringHasExactly("abcdef", 2), false) },
+	() => { assert.equal(stringHasExactly("abcdef", 3), false) },
+	() => { assert.equal(stringHasExactly("bababc", 2), true) },
+	() => { assert.equal(stringHasExactly("bababc", 3), true) },
+	() => { assert.equal(stringHasExactly("abbcde", 2), true) },
+	() => { assert.equal(stringHasExactly("abbcde", 3), false) },
+	() => { assert.equal(stringHasExactly("abcccd", 2), false) },
+	() => { assert.equal(stringHasExactly("abcccd", 3), true) },
+	() => { assert.equal(stringHasExactly("aabcdd", 2), true) },
+	() => { assert.equal(stringHasExactly("aabcdd", 3), false) },
+	() => { assert.equal(stringHasExactly("abcdee", 2), true) },
+	() => { assert.equal(stringHasExactly("abcdee", 3), false) },
+	() => { assert.equal(stringHasExactly("ababab", 2), false) },
+	() => { assert.equal(stringHasExactly("ababab", 3), true) },
+	testCases.create(solution1, [
+		"abcdef",
+		"bababc",
+		"abbcde",
+		"abcccd",
+		"aabcdd",
+		"abcdee",
+		"ababab"
+	], 12)
 ];
 
 // Run the functions.
