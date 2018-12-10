34281978e23a8f011104729d4ff965d5ee2df9a7
Morris, Scott <SMorris@victoria.com>
2018-12-09 19:25:45 -0500

    Day 8 initial commit

---
+++ 2018/day08/index.js
@@ -0,0 +1,48 @@
+// http://adventofcode.com/2018/day/8
+"use strict"
+
+// Libraries.
+
+const assert = require("assert");
+
+// Dependencies.
+
+const getInput = require("../helpers/get-input");
+const logger = require("../helpers/logger");
+const testCases = require("../helpers/test-cases");
+
+// Private.
+
+/* put private functions here */
+
+// Solution #1
+const solution1 = (input) => {
+	/* put code to solve for part #1 here. */
+};
+
+// Solution #2
+const solution2 = (input, answer1) => {
+	/* put code to solve for part #2 here. */
+};
+
+// Public
+
+// Define the tests that need to pass before running.
+const tests = [
+	() => { assert.equal(true, true) },
+	testCases.create(solution1, "input", "output")
+];
+
+// Run the functions.
+const run = (input = getInput(__dirname)) => {
+	const answer1 = solution1(input);
+	const answer2 = solution2(input, answer1);
+
+	// Display results.
+	logger.displayResults(answer1, answer2);
+};
+
+module.exports = {
+	tests,
+	run
+};
